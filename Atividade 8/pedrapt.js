a1b56e0bb171a7f8fb8c64ac93819851a2fa87a2
rebecafatec <111204513+rebecafatec@users.noreply.github.com>
2023-09-25 12:23:11 -0300

    Add files via upload

---
+++ Atividade 8/pedrapt.js
@@ -0,0 +1,28 @@
+function inicia(){
+    var opcao=parseInt(prompt("Escolha sua opção:  1 - Papel | 2 - Pedra | 3 - Tesoura"));
+    var resposta=Math.floor((Math.random() * 3) + 1);
+    var voceescolheu = "";
+    var verificador=true;
+
+    if(resposta==1){
+        opcaoe+="Pedra";
+    }
+    else if(resposta==2){
+        opcaoe+="Papel";
+    }
+    else if(resposta==3){
+        opcaoe+="Tesoura";
+    }
+
+    if(opcao===resposta){
+        alert("Empate"+voceescolheu+".");
+    }else if(opcao==1 && resposta==3){
+        alert("Pedra quebra tesoura."+voceescolheu+".");
+    }else if(opcao==3 && resposta==2){
+        alert("Tesoura corta papel."+voceescolheu+".");
+    }else if(opcao==2 && resposta==1){
+        alert("Papel cobre a pedra."+voceescolheu+".");
+    }
+}
+
+inicia();
